6f561afe9264f5511fe4c2558104760295ad9cb0
Jeff Naruchitparames <jnaruchit@gmail.com>
2013-03-03 11:10:48 -0800

    Minor refactor on Users Controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,11 @@ class UsersController < ApplicationController
     if params[:query]
       Logger.new(STDOUT).info params[:query]
     end
-    respond_to do |format|
-      format.html
 
-      format.json {
-        render json: (
-          @users
-        )
-      }
+    respond_to do |format|
+      format.json { render json: @users, status: :ok }
+
+      format.html
     end
   end
 end
