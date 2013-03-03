12e923e8061f9125339cedd9fbce8620cb67267a
Jeff Naruchitparames <jnaruchit@gmail.com>
2013-03-03 11:13:11 -0800

    Refactor Skills Controller.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,10 +2,19 @@ class SkillsController < ApplicationController
   
   def index
     @user = current_user
+
+    respond_to do |format|
+      format.json
+      format.html
+    end
   end
   
   def import
     session[:"user_return_to"] = skills_path
-    redirect_to user_omniauth_authorize_path(:linkedin)
+
+    respond_to do |format|
+      format.json
+      format.html { redirect_to user_omniauth_authorize_path(:linkedin) }
+    end
   end
 end
