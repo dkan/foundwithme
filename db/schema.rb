dc22f9130576ead0cd6e962324d2d2165db73609
luis.balceda <lbalceda@gmail.com>
2013-03-02 16:58:55 -0800

    add index on authentications:uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302233838) do
+ActiveRecord::Schema.define(:version => 20130303005641) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20130302233838) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "authentications", ["uid"], :name => "index_authentications_on_uid"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
