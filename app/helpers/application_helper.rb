5b27e33e29bd975a4072ccbf0d89c3067de05cc5
luis.balceda <lbalceda@gmail.com>
2013-03-04 00:55:16 -0800

    user edit now in partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,13 @@ module ApplicationHelper
       { id: us.skill.id, name: us.skill.name }
     end
   end
+  
+  def interests_for (user)
+    user.user_interests.includes(:interest).map do |us|
+      { id: us.interest.id, name: us.interest.name }
+    end
+  end
+  
   def body_class
     [controller_name, action_name].join('-')
   end
