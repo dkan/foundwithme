f885cac9dbb94711cec58910126e31594b1d64e0
Damien <damien.kan@gmail.com>
2013-03-04 00:36:06 -0800

    Clicking on glyphcon also toggles the check box for search fields

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -78,12 +78,18 @@ $(document).ready(function () {
   });
 
   $('div[id^=search-]').on('click', function (event) {
-
     if ($(event.target).is('span.checkbox')) {
       if ($(event.target).hasClass('checked')) {
         $(event.target).removeClass('checked');
       } else {
         $(event.target).addClass('checked');
+      }
+    }
+    if ($(event.target).is('span.checkbox i')) {
+      if ($(event.target).parent().hasClass('checked')) {
+        $(event.target).parent().removeClass('checked');
+      } else {
+        $(event.target).parent().addClass('checked');
       }
     }
   });
